de7b10c9f0041add706332d64f88c5b23dc581b4
loicambamany <ambamanyloic@gmail.com>
2021-03-25 15:19:26 -0400

    commit for BST Feb 23

---
+++ src/main/java/com/loic/projectfacebook/leetcode/medium/RemoveNthNodeEndList.java
@@ -0,0 +1,15 @@
+package com.loic.projectfacebook.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class RemoveNthNodeEndList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode node = head;
+
+
+        return node;
+    }
+}
